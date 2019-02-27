ce1185ba49ede5c0f3a3288f33dcaf589ddcbc7a
David Whynot <davidmwhynot@gmail.com>
2019-02-26 19:10:41 -0500

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+const AUTOPREFIXER_BROWSERS = [
+	'> 1%',
+	'last 2 versions'
+];
+
+
+gulp.task('sass',
+	gulp.series(() => {
+		return gulp.src('src/public/sass/*.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+			.pipe(gulp.dest('dist/public'));
+	})
+);
+
+gulp.task('default',
+	gulp.watch('src/public/sass/**/*.scss', gulp.series('sass'));
+);
