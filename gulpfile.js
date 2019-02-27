8a13fdc0802811009665a54e83313d12f1d64bab
David Whynot <davidmwhynot@gmail.com>
2019-02-26 19:20:53 -0500

    bugfixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
+const AUTOPREFIXER_BROWSERS = ["> 1%", "last 2 versions"];
 
-const AUTOPREFIXER_BROWSERS = [
-	'> 1%',
-	'last 2 versions'
-];
-
-
-gulp.task('sass',
-	gulp.series(() => {
-		return gulp.src('src/public/sass/*.scss')
-			.pipe(sass().on('error', sass.logError))
-			.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-			.pipe(gulp.dest('dist/public'));
-	})
+gulp.task(
+  "sass",
+  gulp.series(() => {
+    return gulp
+      .src("src/public/sass/*.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+      .pipe(gulp.dest("dist/public"));
+  })
 );
 
-gulp.task('default',
-	gulp.watch('src/public/sass/**/*.scss', gulp.series('sass'));
+gulp.task(
+  "default",
+  gulp.series(() => {
+    gulp.watch("src/public/sass/**/*.scss", gulp.series("sass"));
+  })
 );
