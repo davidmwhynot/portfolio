c5906f7e65739a72706479abc3f5ee42a1e44843
David Whynot <davidmwhynot@gmail.com>
2019-04-19 15:34:53 -0400

    sync commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,43 @@
+// imports
 const gulp = require("gulp");
+
+const embedSvg = require("gulp-embed-svg");
+
+const srcmaps = require("gulp-sourcemaps");
 
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 
+// config
 const AUTOPREFIXER_BROWSERS = ["> 1%", "last 2 versions"];
 
-gulp.task(
-  "sass",
-  gulp.series(() => {
-    return gulp
-      .src("src/public/sass/*.scss")
-      .pipe(sass().on("error", sass.logError))
-      .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-      .pipe(gulp.dest("dist/public"));
-  })
-);
+// tasks
+gulp.task("html", () => {
+  return gulp
+    .src("src/public/*.html")
+    .pipe(embedSvg({ root: "./src/public/media" }))
+    .pipe(gulp.dest("dist/public"));
+});
+
+gulp.task("sass", () => {
+  return gulp
+    .src("src/public/sass/*.scss")
+    .pipe(srcmaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(srcmaps.write())
+    .pipe(gulp.dest("dist/public"));
+});
+
+gulp.task("media", () => {
+  return gulp.src("src/public/media/**/*").pipe(gulp.dest("dist/public/media"));
+});
 
 gulp.task(
   "default",
-  gulp.series(() => {
+  gulp.series(gulp.parallel("sass", "media", "html"), () => {
     gulp.watch("src/public/sass/**/*.scss", gulp.series("sass"));
+    gulp.watch("src/public/media/**/*", gulp.series("media"));
+    gulp.watch("src/public/*.html", gulp.series("html"));
   })
 );
